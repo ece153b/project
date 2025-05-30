826d03133bf0533a9e28639fbfa6f56768a37f48
Vishal S. <67886538+VishalSeeni-108@users.noreply.github.com>
2025-05-29 18:58:47 -0700

    Add files via upload

--- Part_B_and_C/src/DMA.c
+++ Part_B_and_C/src/DMA.c
@@ -10,5 +10,43 @@
 #include "SysTimer.h"
 
 void DMA_Init_UARTx(DMA_Channel_TypeDef * tx, USART_TypeDef * uart) {
-	//TODO
+	//Enable the clock for DMA
+	RCC->AHB1ENR |= RCC_AHB1ENR_DMA1EN; 
+	//Wait 20us for DMA to finish setting up
+	delay(20);  	
+	//Disable Channel 6
+	DMA1_Channel6->CCR &= ~(DMA_CCR_EN);
+	//Set Memory-to-Memory mode
+	DMA1_Channel6->CCR |= DMA_CCR_MEM2MEM;
+	//Set channel priority to high (10)
+	DMA1_Channel6->CCR &= ~(DMA_CCR_PL); 
+	DMA1_Channel6->CCR |= DMA_CCR_PL_1;
+	//Set peripheral size to 32 bit (10)
+	DMA1_Channel6->CCR &= ~(DMA_CCR_PSIZE); 
+	DMA1_Channel6->CCR |= DMA_CCR_PSIZE_1; 
+	//Set memory size to 32-bit (10)
+	DMA1_Channel6->CCR &= ~(DMA_CCR_MSIZE); 
+	DMA1_Channel6->CCR |= DMA_CCR_MSIZE_1; 
+	//Disable peripheral increment mode
+	DMA1_Channel6->CCR &= ~(DMA_CCR_PINC); 
+	//Enable memory increment mode
+	DMA1_Channel6->CCR |= DMA_CCR_MINC; 
+	//Disable circular mode
+	DMA1_Channel6->CCR &= ~(DMA_CCR_CIRC); 
+	//Set data transfer direction to Memory-to-Peripheral
+	DMA1_Channel6->CCR |= DMA_CCR_DIR; 
+	//Set the data source to data buffer provided in CRC.h
+	DMA1_Channel6->CMAR = (uint32_t)DataBuffer; //TODO - change data source
+	//Set the data destination to the data register of the CRC block
+	DMA1_Channel6->CPAR = (uint32_t)&(CRC->DR); //TODO - change data destination
+	//Disable half transfer interrupt
+	DMA1_Channel6->CCR &= ~(DMA_CCR_HTIE); 
+	//Disable transfer error interrupt
+	DMA1_Channel6->CCR &= ~(DMA_CCR_TEIE); 
+	//Enable transfer complete interrupt
+	DMA1_Channel6->CCR |= DMA_CCR_TCIE;
+	//Set interrupt priority to 0 in NVIC
+	NVIC_SetPriority(DMA1_Channel6_IRQn, 0);
+	//Enable interrupt in NVIC
+	NVIC_EnableIRQ(DMA1_Channel6_IRQn);
 }
--- Part_B_and_C/src/UART.c
+++ Part_B_and_C/src/UART.c
@@ -29,23 +29,98 @@ void transfer_data(char ch);
 void on_complete_transfer(void);
 
 void UART1_Init(void) {
-	//TODO
+	//Enable clock in peripheral clock register
+	RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+	//Select the system clock as USART1 clock source in peripheral independent clock config register
+	RCC->CCIPR &= ~(RCC_CCIPR_USART1SEL);
+	RCC->CCIPR |= RCC_CCIPR_USART1SEL_0;
 }
 
 void UART2_Init(void) {
-	//TODO
+	//Enable clock in peripheral clock register
+	RCC->APB1ENR1 |= RCC_APB1ENR1_USART2EN;
+	//Select the system clock as USART2 clock source in peripheral independent clock config register
+	RCC->CCIPR &= ~(RCC_CCIPR_USART2SEL);
+	RCC->CCIPR |= RCC_CCIPR_USART2SEL_0;
 }
 
 void UART1_GPIO_Init(void) {
-	//TODO
+	//Enable Clock for PA 6 (tx/transmitter) and Pa 7 (RX/reciever)
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
+	//Clear mode reg
+	GPIOB->MODER &= ~(GPIO_MODER_MODE6);
+	GPIOB->MODER |= GPIO_MODER_MODE6_1; //double check
+	GPIOB->MODER &= ~(GPIO_MODER_MODE7);
+	GPIOB->MODER |= GPIO_MODER_MODE7_1; //double check
+	
+	//Choose alternate function
+	GPIOB->AFR[0] |= GPIO_AFRL_AFSEL6; 
+	GPIOB->AFR[0] &= ~(GPIO_AFRL_AFSEL6_3); //Set AF for PB6 to AF7 (0111)
+	GPIOB->AFR[0] |= GPIO_AFRL_AFSEL7; 
+	GPIOB->AFR[0] &= ~(GPIO_AFRL_AFSEL7_3); //Set AF for PB7 to AF7 (0111)
+	
+	//Operate at high speed
+	GPIOB->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEED6);
+	GPIOB->OSPEEDR |= GPIO_OSPEEDR_OSPEED6_1;
+	GPIOB->OSPEEDR &= ~GPIO_OSPEEDR_OSPEED7;
+	GPIOB->OSPEEDR |= GPIO_OSPEEDR_OSPEED7_1;
+	//Push-pull output type
+	GPIOB->OTYPER &= ~(GPIO_OTYPER_OT6);
+	GPIOB->OTYPER &= ~(GPIO_OTYPER_OT7);
+	//Configure both GPIO pins to use pull-up registors for I/O
+	GPIOB->PUPDR &= ~(GPIO_PUPDR_PUPD6);
+	GPIOB->PUPDR |= GPIO_PUPDR_PUPD6_0;
+	GPIOB->PUPDR &= ~(GPIO_PUPDR_PUPD7);
+	GPIOB->PUPDR |= GPIO_PUPDR_PUPD7_0;
 }
 
 void UART2_GPIO_Init(void) {
-	//TODO
+	//TODO//Enable Clock for PA 2 (tx/transmitter) and Pa 3 (RX/reciever)
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+	//Clear mode reg
+	GPIOA->MODER &= ~(GPIO_MODER_MODE2);
+	GPIOA->MODER |= GPIO_MODER_MODE2_1; //double check
+	GPIOA->MODER &= ~(GPIO_MODER_MODE3);
+	GPIOA->MODER |= GPIO_MODER_MODE3_1; //double check
+	//Choose alternate function
+	GPIOA->AFR[0] |= GPIO_AFRL_AFSEL2; 
+	GPIOA->AFR[0] &= ~(GPIO_AFRL_AFSEL2_3); //Set AF for PA2 to AF7 (0111)
+	GPIOA->AFR[0] |= GPIO_AFRL_AFSEL3; 
+	GPIOA->AFR[0] &= ~(GPIO_AFRL_AFSEL3_3); //Set AF for PA3 to AF7 (0111)
+	
+	
+		
+	//Operate at high speed
+	GPIOA->OSPEEDR |= GPIO_OSPEEDR_OSPEED2;
+	GPIOA->OSPEEDR |= GPIO_OSPEEDR_OSPEED3;
+	//Push-pull output type
+	GPIOA->OTYPER &= ~(GPIO_OTYPER_OT2);
+	GPIOA->OTYPER &= ~(GPIO_OTYPER_OT3);
+	//Configure both GPIO pins to use pull-up registors for I/O
+	GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPD2);
+	GPIOA->PUPDR |= GPIO_PUPDR_PUPD2_0;
+	GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPD3);
+	GPIOA->PUPDR |= GPIO_PUPDR_PUPD3_0;
 }
 
 void USART_Init(USART_TypeDef * USARTx) {
-	//TODO
+	// Disable USART before configuring settings
+	USARTx->CR1 &= ~USART_CR1_UE;
+	
+	// Set Communication Parameters
+	USARTx->CR1 &= ~(USART_CR1_M);     // 00 -> 8 Data Bits
+	USARTx->CR1 &= ~(USART_CR1_OVER8); // 0 -> Oversampling by 16
+	USARTx->CR2 &= ~(USART_CR2_STOP);  // 00 -> 1 Stop Bit
+	
+	// Set Baud Rate
+	// f_CLK = 80 MHz, Baud Rate = 9600 = 80 MHz / DIV -> DIV = 8333 = 0x208D
+	USARTx->BRR = 0x208D;
+	
+	// Enable Transmitter/Receiver
+	USARTx->CR1 |= USART_CR1_TE | USART_CR1_RE;
+	
+	// Enable USART
+	USARTx->CR1 |= USART_CR1_UE;
 }
 
 /**
